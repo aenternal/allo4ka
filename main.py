05ad675ef6cec233c3884015e308cd63ad9aa9b5
Ilya <kotikote875@gmail.com>
2022-05-16 02:47:35 +0300

    разбиение на функции не помогло

--- main.py
+++ main.py
@@ -11,17 +11,24 @@ bot = Bot(token=token)
 
 
 @bot.on.message(text='<msg>')
-async def hygiene(message: Message):
+async def hygiene(m: Message):
     if randint(1, 5) == 2:
-        msg = gen()
-        await message.answer(msg)
+        await textonator(m)
     elif randint(1, 5) == 3:
-        await wavgen()
-        time.sleep(10)
-        voice_msg = VoiceMessageUploader(bot.api)
-        voice = await voice_msg.upload(" ", "test.wav", peer_id=message.peer_id)
-        await message.answer('', attachment=voice)
+        await voicenator(m)
 
+
+async def voicenator(m: Message):
+    await wavgen()
+    time.sleep(10)
+    voice_msg = VoiceMessageUploader(bot.api)
+    voice = await voice_msg.upload(" ", "test.wav", peer_id=m.peer_id)
+    await m.answer('', attachment=voice)
+
+
+async def textonator(m: Message):
+    msg = gen()
+    await m.answer(msg)
 
 
 
