bc3f746098a514949321cfbbf4e5be619ff6d310
aenternal <kotikote875@gmail.com>
2023-09-07 21:50:04 +0300

    ongoing reengineering

--- src/service.py
+++ src/service.py
@@ -1,17 +1,16 @@
 import torch
 import os
 import markovify
-import silero
 
 
-def wavgen(text: str, speaker: str):
+def wavgen(text: str, speaker: str = 'baya'):
     device = torch.device('cpu')
     torch.set_num_threads(2)
     local_file = 'model.pt'
 
     if not os.path.isfile(local_file):
-        torch.hub.download_url_to_file('https://models.silero.ai/models/tts/ru/ru_v3.pt',
-                                       local_file)
+        torch.hub.download_url_to_file(url='https://models.silero.ai/models/tts/ru/ru_v3.pt',
+                                       dst=local_file)
 
     model = torch.package.PackageImporter(local_file).load_pickle("tts_models", "model")
     model.to(device)
@@ -20,9 +19,8 @@ def wavgen(text: str, speaker: str):
     sample_rate = 44100
 
     model.save_wav(text=text, speaker=speaker, sample_rate=sample_rate)
-    
 
 
-async def gen():
+async def gen_text(text: str):
     text_model = markovify.Text(text)
     return text_model.make_sentence(tries=100)
